001c1451982469dd4c599f8aaa58df6bf190c341
Siwale-J <jayj97588@gmail.com>
2023-07-11 23:49:18 +0200

    00-14

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`serve at http://127.0.0.1:${port}`);
 });
